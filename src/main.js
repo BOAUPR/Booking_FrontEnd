1f23d4839ac49595d70245c5579ba70c126f37bb
Naphat Tisonthi <62160006@go.buu.ac.th>
2022-04-06 22:05:02 +0700

    import vue bootstrap in main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
 import Vue from 'vue'
-import './plugins/bootstrap-vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
-
 Vue.config.productionTip = false
 
 new Vue({
