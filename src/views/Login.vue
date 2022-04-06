5f5d3e86cd7637c4dadfc357ca6907b7cf395986
Naphat Tisonthi <62160006@go.buu.ac.th>
2022-04-06 23:14:09 +0700

    edit Login ui

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,33 +1,58 @@
 <template>
-  <div>
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group id="input-group-1" label="Username:" label-for="input-1">
-        <b-form-input
-          id="input-1"
-          v-model="form.email"
-          type="email"
-          placeholder="Enter Username"
-          required
-        ></b-form-input>
-      </b-form-group>
+  <b-row align-h="center">
+    <b-col cols="4">
+      <b-card
+        title="Login"
+        src="../assets/logo_user.png"
+        tag="article"
+        style="max-width: 40rem"
+        class="mb-2"
+      >
+      <img alt="Vue logo" src="../assets/logo_user.png" width="100" class="mb-2">
+        <b-card-text>
+          <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+            <b-form-group
+              id="input-group-1"
+              label="USERNAME:"
+              label-for="input-1"
+              label-cols="4"
+            >
+              <b-form-input
+                id="input-1"
+                v-model="form.name"
+                type="text"
+                placeholder="Enter Username"
+                required
+              ></b-form-input>
+            </b-form-group>
 
-      <b-form-group id="input-group-2" label="Password:" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="form.password"
-          placeholder="Enter password"
-          required
-          type="password"
-        ></b-form-input>
-      </b-form-group>
+            <b-form-group
+              id="input-group-2"
+              label="PASSWORD:"
+              label-for="input-2"
+              label-cols="4"
+            >
+              <b-form-input
+                id="input-2"
+                v-model="form.password"
+                placeholder="Enter password"
+                required
+                type="password"
+              ></b-form-input>
+            </b-form-group>
 
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
-    </b-form>
-    <b-card class="mt-3" header="Form Data Result">
-      <pre class="m-0">{{ form }}</pre>
-    </b-card>
-  </div>
+            <b-button type="submit" variant="primary" class="m-3"
+              >Login</b-button
+            >
+            <b-button type="reset" variant="danger">Reset</b-button>
+          </b-form>
+          <b-card class="mt-3" header="Form Data Result">
+            <pre class="m-0">{{ form }}</pre>
+          </b-card>
+        </b-card-text>
+      </b-card>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
@@ -35,12 +60,9 @@ export default {
   data () {
     return {
       form: {
-        email: '',
         name: '',
-        food: null,
-        checked: []
+        password: ''
       },
-      foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
       show: true
     }
   },
@@ -51,12 +73,10 @@ export default {
     },
     onReset (event) {
       event.preventDefault()
-      // Reset our form values
-      this.form.email = ''
+
       this.form.name = ''
-      this.form.food = null
-      this.form.checked = []
-      // Trick to reset/clear native browser form validation state
+      this.form.password = ''
+
       this.show = false
       this.$nextTick(() => {
         this.show = true
