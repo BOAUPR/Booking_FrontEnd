108d36f2f468b4f7c5de38a33807fc3cdcdf9c50
Chenpob Nopakiat <62160055@go.buu.ac.th>
2022-04-07 17:54:57 +0700

    add login screen in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,75 @@
 <template>
   <div id="app">
-    <b-container fluid>
+
+    <b-container v-if="!isLogint()">
+      <b-row align-h="center">
+        <b-col cols="4">
+          <b-card
+            title="Login"
+            src="./assets/logo_user.png"
+            tag="article"
+            style="max-width: 40rem"
+            class="mb-2"
+          >
+            <img
+              alt="Vue logo"
+              src="./assets/logo_user.png"
+              width="100"
+              class="mb-2"
+            />
+            <b-card-text>
+              <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+                <b-form-group
+                  id="input-group-1"
+                  label="USERNAME:"
+                  label-for="input-1"
+                  label-cols="4"
+                >
+                  <b-form-input
+                    id="input-1"
+                    v-model="form.name"
+                    type="text"
+                    placeholder="Enter Username"
+                    required
+                  ></b-form-input>
+                </b-form-group>
+
+                <b-form-group
+                  id="input-group-2"
+                  label="PASSWORD:"
+                  label-for="input-2"
+                  label-cols="4"
+                >
+                  <b-form-input
+                    id="input-2"
+                    v-model="form.password"
+                    placeholder="Enter password"
+                    required
+                    type="password"
+                  ></b-form-input>
+                </b-form-group>
+
+                <b-button type="submit" variant="primary" class="m-3" @click="login()"
+                  >Login</b-button
+                >
+                <b-button type="reset" variant="danger">Reset</b-button>
+              </b-form>
+              <b-card class="mt-3" header="Form Data Result">
+                <pre class="m-0">{{ form }}</pre>
+              </b-card>
+              {{ $store.state.auth.user }}
+              {{ $store.getters['auth/isLogin'] }}
+            </b-card-text>
+          </b-card>
+
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-container fluid v-if="isLogint()">
       <b-row>
         <b-col cols="2" class="app-menu min-vh-100">
-          <Menu/>
+          <Menu />
         </b-col>
         <b-col cols="10">
           <b-container>
@@ -26,8 +92,27 @@
 <script>
 import Menu from './components/Menu.vue'
 export default {
+  data () {
+    return {
+      isLogin: false,
+      form: {
+        name: '',
+        password: ''
+      },
+      show: true
+    }
+  },
   components: {
     Menu
+  },
+  methods: {
+    login () {
+      this.isLogin = true
+    },
+    isLogint () {
+      return this.isLogin
+    }
+
   }
 }
 </script>
