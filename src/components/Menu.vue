630a538ee921555fb98e5955ec20ff13c1ed2df8
Chenpob Nopakiat <62160055@go.buu.ac.th>
2022-04-07 16:52:02 +0700

    test link menu to /about

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -7,11 +7,11 @@
     <br>
     <br>
     <b-nav vertical>
-      <b-nav-item> <b-button class="buttonMenu" variant="outline-secondary"> <b-icon-book></b-icon-book> <br> ตารางการใช้ห้อง </b-button></b-nav-item>
+      <b-nav-item to="/about"> <b-button class="buttonMenu" variant="outline-secondary"> <b-icon-book></b-icon-book> <br> ตารางการใช้ห้อง </b-button></b-nav-item>
       <b-nav-item> <b-button class="buttonMenu" variant="outline-secondary"> สถานะการจอง </b-button></b-nav-item>
       <b-nav-item> <b-button class="buttonMenu" variant="outline-secondary"> รายการจอง </b-button></b-nav-item>
       <b-nav-item> <b-button class="buttonMenu" variant="outline-secondary"> จัดการข้อมูลผู้ใช้ </b-button></b-nav-item>
-      <b-nav-item> <b-button class="buttonMenu" variant="outline-secondary"> ออกจากระบบ </b-button></b-nav-item>
+      <b-nav-item href="http://localhost:8080/"> <b-button class="buttonMenu" variant="outline-secondary"> ออกจากระบบ </b-button></b-nav-item>
     </b-nav>
   </div>
 </template>
