68be678f40cc126dd1dca203e58b8a4895f3347b
Puchong Sumalanukun <62160294@go.buu.ac.th>
2022-04-09 20:00:42 +0700

    Create Management.vue and npm i vue-cal

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,11 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/management',
+    name: 'Management',
+    component: () => import('../views/Management.vue')
   }
 ]
 
